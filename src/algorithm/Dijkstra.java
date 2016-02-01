714b40af63635cbbc9b688a50bc53cdc9c5d196e
jdrumelhart <jdrumelhart@yahoo.com>
2016-02-01 11:55:40 -0800

    Created interface NeedNext with static interface Next

--- src/algorithm/Dijkstra.java
+++ src/algorithm/Dijkstra.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Dijkstra {
-	String starting;
-	ArrayList<Boolean> k;
-	ArrayList<Integer> dv;
-	ArrayList<HashMap<String,String>> pv;
+	private String starting;
+	private ArrayList<Boolean> k;
+	private ArrayList<Integer> dv;
+	private ArrayList<String> nodes;
+	private ArrayList<String> prev;
+	
 	public Dijkstra(HashMap<String, HashMap<String, Integer>> h, String starting) {
 		
 	}
---
+++ src/algorithm/NeedNext.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+import java.util.ArrayList;
+
+public interface NeedNext {
+	public static ArrayList<String> next(String i, String g, Dijkstra d) {
+		return null;
+	}
+}
