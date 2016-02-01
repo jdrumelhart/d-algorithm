3e707a9c641450b04fa7d24d54dd01bbfa39e3c8
jdrumelhart <jdrumelhart@yahoo.com>
2016-02-01 11:46:16 -0800

    Created constructor and arraylists in dijkstra

--- src/algorithm/Dijkstra.java
+++ src/algorithm/Dijkstra.java
@@ -1,1 +1,14 @@
 package algorithm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Dijkstra {
+	String starting;
+	ArrayList<Boolean> k;
+	ArrayList<Integer> dv;
+	ArrayList<HashMap<String,String>> pv;
+	public Dijkstra(HashMap<String, HashMap<String, Integer>> h, String starting) {
+		
+	}
+}
