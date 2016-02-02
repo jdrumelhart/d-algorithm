1c92a37cda7670cb4c03daf0de0f53452dc9e2e0
bdavies01 <bertdaviezz@gmail.com>
2016-02-02 13:13:40 -0800

    Added Mr. Kuszmaul's requirements for the project.

--- src/algorithm/Dijkstra.java
+++ src/algorithm/Dijkstra.java
@@ -1,22 +1,18 @@
 package algorithm;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import graph.Graph;
-import graph.Edge;
-import graph.Node;
+import java.util.HashMap;
 
 public class Dijkstra {
-	private final List<Node> nodes;
-	private final List<Edge> edges;
-	
-	public Dijkstra(Graph graph) {
-		this.nodes = new ArrayList<Node>(graph.getNodes());
-		this.edges = new ArrayList<Edge>(graph.getEdges());
+
+	public ArrayList<String> prev;
+	public String nodes;
+
+	public Dijkstra(HashMap<String, Integer> map, String name) {
+
 	}
-	
+
 	public void findMin() {
-		
+
 	}
 }
