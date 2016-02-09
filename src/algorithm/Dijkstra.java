0c54dadd5c37872d51309069de6e440d2a8505d4
jdrumelhart <jdrumelhart@yahoo.com>
2016-02-09 10:11:35 -0800

    Imported code to test Dijkstra object

--- src/algorithm/Dijkstra.java
+++ src/algorithm/Dijkstra.java
@@ -5,14 +5,13 @@ import java.util.HashMap;
 
 public class Dijkstra {
 
+	//This is the HashMap that will ultimately be accessed by the interfacing program
+	public HashMap<String, String> path;
+	
 	public ArrayList<String> prev;
 	public String nodes;
 
-	public Dijkstra(HashMap<String, Integer> map, String name) {
-
-	}
-	
-	public void findMin() {
+	public Dijkstra(HashMap<String, HashMap<String, Integer>> map, String name) {
 
 	}
 }
