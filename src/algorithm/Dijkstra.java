3b90a02b2d914f296a28ecbd5c968baeb1731bbd
jdrumelhart <jdrumelhart@yahoo.com>
2016-02-09 10:43:50 -0800

    Began initializing data structures

--- src/algorithm/Dijkstra.java
+++ src/algorithm/Dijkstra.java
@@ -8,10 +8,13 @@ public class Dijkstra {
 	//This is the HashMap that will ultimately be accessed by the interfacing program
 	public HashMap<String, String> path;
 	
+	public ArrayList<Boolean> visited;
+	public ArrayList<HashMap<String, Integer>> dist;
 	public ArrayList<String> prev;
-	public String nodes;
+	public ArrayList<String> nodes;
 
 	public Dijkstra(HashMap<String, HashMap<String, Integer>> map, String name) {
-
+		nodes = new ArrayList<String>(map.keySet());
+		dist = new ArrayList<HashMap<String, Integer>>(map.values());
 	}
 }
