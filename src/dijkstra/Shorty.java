d3c711adec2838f913beb2f22d02c3bbedc348f1
Eric Liu <ericxliu@gmail.com>
2016-02-11 22:54:35 -0800

    finished documentation

--- src/dijkstra/Shorty.java
+++ src/dijkstra/Shorty.java
@@ -3,6 +3,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public interface Shorty {
+	
+	/**
+	 * This meets the specifications of having an interface with the Next() method returning an ArrayList of Strings and has the parameters
+	 * of a String and a Dijkstra object.
+	 * 
+	 * @param name the name of the destination node
+	 * @param d the Dijkstra object used which will provide a Graph and the source node.
+	 * @return the shortest path from the source node to the destination one.
+	 */
 	public static ArrayList<String> Next(String name, Dijkstra d) {
 		HashMap<String,String> map = d.path;
 		ArrayList<String> s = new ArrayList<String>();
--- src/dijkstra/Vertex.java
+++ src/dijkstra/Vertex.java
@@ -10,24 +10,48 @@ public class Vertex implements Comparable<Vertex> {
 	//The surrounding vertices
 	public ArrayList<Edge> neighbors;
 	
+	//The path
 	public LinkedList<Vertex> path;
+	
+	//minDistance starts off extremely high to ensure the actual minimum is found later.
 	public double minDistance = Double.POSITIVE_INFINITY;
+	
+	//The previous node visited
 	public Vertex previous;
 
+	/**
+	 * Compares the min distance of this vertex with the min distance of the other vertex. 
+	 * This is done by delegating to Java's Double class.
+	 * 
+	 * @param other the vertex to be compared with this one
+	 */
 	public int compareTo(Vertex other) {
 		return Double.compare(minDistance, other.minDistance);
 	}
 
+	/**
+	 * Creates a new Vertex with the specified name. 
+	 * 
+	 * @param name the name of this vertex
+	 */
 	public Vertex(String name) {
 		this.name = name;
 		neighbors = new ArrayList<Edge>();
 		path = new LinkedList<Vertex>();
 	}
 	
+	/**
+	 * Sets the last visited Vertex to the specified one.
+	 * 
+	 * @param v the previous vertex visited
+	 */
 	public void addPrev(Vertex v) {
 		previous = v;
 	}
 
+	/**
+	 * Returns the name of this Vertex.
+	 */
 	@Override
 	public String toString() {
 		return name;
